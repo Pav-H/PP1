5679acb9fcea3984f49b110252c0f3330da4b106
Pav-H <p.khomutov@gmail.com>
2023-07-19 20:29:37 +0300

    FIX: hibernate configuration using properties instead of xml

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -2,13 +2,17 @@ package jm.task.core.jdbc.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 import java.sql.SQLException;
 
+import jm.task.core.jdbc.model.User;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
 
 public class Util {
 	// JDBC
@@ -20,17 +24,34 @@ public class Util {
     }
 
 	// Hibernate
-	private static final SessionFactory sessionFactory = buildSessionFactory();
+	private static SessionFactory sessionFactory = buildSessionFactory();
 
 	private static SessionFactory buildSessionFactory() {
 		try {
-			StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
-					.configure("hibernate.cfg.xml")
-					.build();
-			Metadata metadata = new MetadataSources(standardRegistry)
-					.getMetadataBuilder()
-					.build();
-			return metadata.getSessionFactoryBuilder().build();
+//			StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
+//					.configure("hibernate.cfg.xml")
+//					.build();
+//			Metadata metadata = new MetadataSources(standardRegistry)
+//					.getMetadataBuilder()
+//					.build();
+//			return metadata.getSessionFactoryBuilder().build();
+
+			Properties properties = new Properties();
+
+			properties.setProperty(Environment.DRIVER, "org.postgresql.Driver");
+			properties.setProperty(Environment.URL, "jdbc:postgresql://localhost:5432/PP1");
+			properties.setProperty(Environment.USER, "postgres");
+			properties.setProperty(Environment.PASS, "nnvq1277");
+			properties.setProperty(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
+			properties.setProperty(Environment.SHOW_SQL, "true");
+			properties.setProperty(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+			properties.setProperty(Environment.HBM2DDL_AUTO, "");
+
+			sessionFactory = new Configuration()
+					.addAnnotatedClass(User.class)
+					.addProperties(properties)
+					.buildSessionFactory();
+			return sessionFactory;
 		} catch (Exception e) {
 			System.out.println("Initial SessionFactory creation failed." + e.getMessage());
 			throw new ExceptionInInitializerError(e);
